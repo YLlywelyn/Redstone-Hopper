4a0631140b2417fd8c9dcddc1074e41cf8019219
Ylva Llywelyn <y.llywelyn@gmail.com>
2024-03-14 20:27:37 +0000

    .

---
+++ src/main/java/uk/fallingsloth/mc/redstone_hopper/RedstoneHopperDataGenerator.java
@@ -0,0 +1,11 @@
+package uk.fallingsloth.mc.redstone_hopper;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RedstoneHopperDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
